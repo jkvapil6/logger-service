255c3a75a411a163dc71af8e7e51b49c3c319633
Jan Kvapil <jkvapil6sa@gmail.com>
2021-12-20 13:54:57 +0100

    delete request

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const fs = require('fs/promises')
+
 // const jsonParser = bodyParser.json()
 
 // app.use(bodyParser.urlencoded())
@@ -12,44 +14,62 @@ const bodyParser = require('body-parser')
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-const port = 3000
+const PORT = 3333
+const DB_PATH = './logs.json'
 
 const LOGTYPE_INFO = 'info'
 const LOGTYPE_WARNING = 'warning'
 const LOGTYPE_SUCCESS = 'success'
 
-const logs = [{
-  id: 0,
-  type: LOGTYPE_INFO, 
-  timestamp: Date.now().valueOf(),
-  message: "Test"
-}]
+const logs = []
+
+///
+/// Initialize db with some random log data
+///
+const initLogs = async () => {
+  const logTypes = [LOGTYPE_INFO, LOGTYPE_WARNING, LOGTYPE_SUCCESS]
+  for (let i = 1; i < 1000; i++) {
+    const rand = Math.floor(Math.random() * 10)
+    logs.push({
+      id: i,
+      type: logTypes[(i + rand) % logTypes.length], 
+      timestamp: Date.now().valueOf(),
+      message: `Test log ${i}`
+    })
+  }
+  
+  const logsStr = JSON.stringify(logs)
+  await fs.writeFile(DB_PATH, logsStr, "utf8")
+}
+
 
 ///
 /// Return all logs
 /// 
-app.get('/all', (req, res) => {
-  res.send(logs)
+app.get('/all', async (req, res) => {
+  try {
+    const all = await fs.readFile(DB_PATH, "utf-8")
+    res.status(200).send(all)
+  } catch (error) {
+    res.status(500).send({ error })
+  }
 })
-
-
-///
-///
-///
 
 
 ///
 /// Delete log by id
 ///
 app.post('/delete', async (req, res) => {
-  console.log(req)
-  
-
   try {
     const { id } = req.body 
+    const all = await fs.readFile(DB_PATH, "utf-8")
+    const logs = JSON.parse(all)
+    const filtered = logs.filter(log => log.id != id)
+    console.log(`Deleting log with id ${id}...`)
 
-
-    console.log(id)
+    const logsStr = JSON.stringify(filtered)
+    await fs.writeFile(DB_PATH, logsStr, "utf8")
+    res.status(200).send({ id })
   } catch (error) {
     res.status(500).send({ error })
   }
@@ -60,6 +80,7 @@ app.post('/delete', async (req, res) => {
 ///
 /// Run server
 ///
-app.listen(port, () => {
-  console.log(`Log service listening at http://localhost:${port}`)
+initLogs()
+app.listen(PORT, async () => {
+  console.log(`Log service listening at http://localhost:${PORT}`)
 })
