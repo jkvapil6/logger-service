f039072d6980388c11d905e5c87bc65485f1848e
Jan Kvapil <jkvapil6sa@gmail.com>
2021-12-20 14:22:43 +0100

    undefined id error handling

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser')
 const fs = require('fs/promises')
-
-// const jsonParser = bodyParser.json()
-
-// app.use(bodyParser.urlencoded())
-// app.use(bodyParser.json({  type: "*/*" })) 
-
-// app.use(bodyParser.json())
-// app.use(bodyParser.raw())
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -21,13 +12,12 @@ const LOGTYPE_INFO = 'info'
 const LOGTYPE_WARNING = 'warning'
 const LOGTYPE_SUCCESS = 'success'
 
-const logs = []
-
 ///
 /// Initialize db with some random log data
 ///
 const initLogs = async () => {
   const logTypes = [LOGTYPE_INFO, LOGTYPE_WARNING, LOGTYPE_SUCCESS]
+  const logs = []
   for (let i = 1; i < 1000; i++) {
     const rand = Math.floor(Math.random() * 10)
     logs.push({
@@ -49,7 +39,56 @@ const initLogs = async () => {
 app.get('/all', async (req, res) => {
   try {
     const all = await fs.readFile(DB_PATH, "utf-8")
-    res.status(200).send(all)
+    const parsed = JSON.parse(all)
+    res.status(200).send(parsed)
+  } catch (error) {
+    res.status(500).send({ error })
+  }
+})
+
+
+///
+/// Return success logs
+/// 
+app.get('/success', async (req, res) => {
+  try {
+    const all = await fs.readFile(DB_PATH, "utf-8")
+    const logs = JSON.parse(all)
+    const filtered = logs.filter(log => log.type == LOGTYPE_SUCCESS)
+
+    res.status(200).send(filtered)
+  } catch (error) {
+    res.status(500).send({ error })
+  }
+})
+
+
+///
+/// Return info logs
+/// 
+app.get('/info', async (req, res) => {
+  try {
+    const all = await fs.readFile(DB_PATH, "utf-8")
+    const logs = JSON.parse(all)
+    const filtered = logs.filter(log => log.type == LOGTYPE_INFO)
+
+    res.status(200).send(filtered)
+  } catch (error) {
+    res.status(500).send({ error })
+  }
+})
+
+
+///
+/// Return warning logs
+/// 
+app.get('/warning', async (req, res) => {
+  try {
+    const all = await fs.readFile(DB_PATH, "utf-8")
+    const logs = JSON.parse(all)
+    const filtered = logs.filter(log => log.type == LOGTYPE_WARNING)
+
+    res.status(200).send(filtered)
   } catch (error) {
     res.status(500).send({ error })
   }
@@ -62,6 +101,10 @@ app.get('/all', async (req, res) => {
 app.post('/delete', async (req, res) => {
   try {
     const { id } = req.body 
+    if (typeof(id) === "undefined") {
+      res.status(400).send({ error: "Missing ID of deleted log" })
+      return
+    }
     const all = await fs.readFile(DB_PATH, "utf-8")
     const logs = JSON.parse(all)
     const filtered = logs.filter(log => log.id != id)
@@ -73,8 +116,6 @@ app.post('/delete', async (req, res) => {
   } catch (error) {
     res.status(500).send({ error })
   }
-
-  res.send(logs)
 })
 
 ///
